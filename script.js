f85342c52cb682efa28ebcb72fa90ad73305b6bf
Deva Palanisamy <dev.palanisamy@gmail.com>
2024-12-08 16:49:01 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("waitlistForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const emailInput = document.getElementById("email");
+    if (emailInput.value) {
+        document.getElementById("successMessage").classList.remove("hidden");
+        emailInput.value = "";
+    }
+});
